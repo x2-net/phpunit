09ba1b988ed5b295e767cb2e5a984e3881c60e8d
Süleyman <developer@topaloglu.eu>
2019-11-19 13:59:34 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suleymantopaloglu
+ * Date: 2019-11-18
+ * Time: 12:56
+ */
---
+++ tests/ArticleTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suleymantopaloglu
+ * Date: 2019-11-18
+ * Time: 13:03
+ */
+
+
+
+class ArticleTest extends \PHPUnit\Framework\TestCase
+{
+
+        /**
+         *
+         */
+        public function testGetName()
+        {
+                $this->assertTrue(true, "Alles Good" );
+        }
+}
